aec5197627628a6e91c316f7dc97122aea32aaad
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-05-01 09:54:28 +0100

    cocktail refined to look out for null lists and also the bubbling part has been separated to meet up with betty requirements

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -24,14 +24,62 @@ void swap_node(listint_t **list, listint_t *node1, listint_t *node2)
 }
 
 /**
+ * bubble_cocktail - The bubbling aspect separated out of the main function
+ *
+ * @list: The list we are sorting
+ * @node: address of the node we are getting so changes can reflect
+ * @direction: An integer flag to decide the direction of the bubble
+ * Return: Either 1 or 0 for if a swap was made or not respectively
+ */
+
+int bubble_cocktail(listint_t **list, listint_t **node, int direction)
+{
+	int swapped;
+	listint_t *next;
+
+	swapped = 0;
+	if (direction == 1)
+	{
+		while ((*node)->next)
+		{
+			next = (*node)->next;
+			if ((*node)->n > ((*node)->next)->n)
+			{
+				swap_node(list, (*node), (*node)->next);
+				swapped = 1;
+				print_list(*list);
+			}
+			else
+				(*node) = next;
+		}
+	}
+	else if (direction == -1)
+	{
+		while ((*node)->prev)
+		{
+			next = (*node)->prev;
+			if ((*node)->n < ((*node)->prev)->n)
+			{
+				swap_node(list, (*node)->prev, (*node));
+				swapped = 1;
+				print_list(*list);
+			}
+			else
+				(*node) = next;
+		}
+	}
+	return (swapped);
+}
+
+/**
  * cocktail_sort_list - A sorting scheme using the cocktail shaker algorithm
  *
  * @list: A linked list we want to sort
-*/
+ */
 
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *node, *next;
+	listint_t *node;
 	int swapped, direction;
 
 	if (!list || !*list)
@@ -39,38 +87,7 @@ void cocktail_sort_list(listint_t **list)
 	node = *list;
 	direction = 1;
 	do {
-		swapped = 0;
-		if (direction == 1)
-		{
-			while (node->next)
-			{
-				next = node->next;
-				if (node->n > (node->next)->n)
-				{
-					swap_node(list, node, node->next);
-					swapped = 1;
-					print_list(*list);
-				}
-				else
-				node = next;
-			}
-		}
-		else if (direction == -1)
-		{
-			while (node->prev)
-			{
-				next = node->prev;
-				if (node->n < (node->prev)->n)
-				{
-					swap_node(list, node->prev, node);
-					swapped = 1;
-					print_list(*list);
-				}
-				else
-				node = next;
-			}
-
-		}
+		swapped = bubble_cocktail(list, &node, direction);
 		direction = direction * -1;
 	} while (swapped);
 }
